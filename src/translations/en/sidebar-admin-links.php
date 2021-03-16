bc936d75389434663039a41c06733eb178198d83
Dan Ott <dan@dtott.com>
2021-03-16 14:17:56 -0400

    Initial commit from pluginfactory.io

---
+++ src/translations/en/sidebar-admin-links.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Sidebar Admin Links plugin for Craft CMS 3.x
+ *
+ * Add links from Settings directly to the sidebar for admin users
+ *
+ * @link      https://dtott.com
+ * @copyright Copyright (c) 2021 Dan Ott
+ */
+
+/**
+ * Sidebar Admin Links en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('sidebar-admin-links', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Dan Ott
+ * @package   SidebarAdminLinks
+ * @since     1.0.0
+ */
+return [
+    'Sidebar Admin Links plugin loaded' => 'Sidebar Admin Links plugin loaded',
+];
